05e4523004454d510f70a1a43ea3eebfa963130d
ZhannaDesyatnikova <149164861+ZhannaDesyatnikova@users.noreply.github.com>
2023-10-27 20:40:50 +0200

    Delete configuration.py

--- configuration.py
+++
@@ -1,3 +0,0 @@
-URL_SERVICE = "https://d022c137-f773-40f7-b119-967684e831e5.serverhub.praktikum-services.ru"  # адрес сервера
-CREATE_ORDER = "/api/v1/orders"  # создание заказа
-ORDER_INFORMATION = "/api/v1/orders/track?t="  # получить информацию о заказе по его номеру
